ddf15345aa84daa455108ee15801e9a3c82934ec
deviantony <anthony.lapenna@portainer.io>
2024-12-09 15:18:30 +1300

    base: update post-start.sh script

--- post-start.sh
+++ post-start.sh
@@ -4,4 +4,8 @@
 
 # We clean-up the tmp folder as part of the startup because it can 
 # be flooded very fast with files and folders related to vscode, devextension, yarn, gobuilds etc...
-rm -rf /tmp/*
+# This removes all the files in the /tmp folder that do not start with vscode, we keep these one
+# as they are needed by the devcontainer extension.
+# Note: this can lead to flooding of vscode* files in the /tmp folder, until a better solution
+# is found, you can periodically clean the tmp folder via rm -rf /tmp/ before stopping the devcontainer.
+find /tmp -mindepth 1 ! -name 'vscode*' -exec rm -rf {} +
