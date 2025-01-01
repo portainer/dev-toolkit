a1b9443db5db12a5112731fcefe00993d4bd9dca
deviantony <anthony.lapenna@portainer.io>
2025-01-02 11:39:48 +1300

    alapenna: add pbkit.vscode-pbkit extension

--- post-start.sh
+++ post-start.sh
@@ -8,4 +8,8 @@
 # as they are needed by the devcontainer extension.
 # Note: this can lead to flooding of vscode* files in the /tmp folder, until a better solution
 # is found, you can periodically clean the tmp folder via rm -rf /tmp/ before stopping the devcontainer.
+
+# TODO: this seems to be breaking the ability to use apt update in the container.
+# Maybe a smarter approach would be to clean all files that are older than x days. To be tested.
+# find /tmp -mindepth 1 -mtime +7 -exec rm -rf {} +
 find /tmp -mindepth 1 ! -name 'vscode*' -exec rm -rf {} +
