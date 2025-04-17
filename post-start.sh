ffdb4ac7647d2025b6518e9ad92e027d82853fa0
deviantony <anthony.lapenna@portainer.io>
2025-04-17 04:05:57 +0000

    build: update Dockerfile and post-start script for version upgrades and cleanup timing

--- post-start.sh
+++ post-start.sh
@@ -4,5 +4,5 @@
 
 # We clean-up the tmp folder as part of the startup because it can 
 # be flooded very fast with files and folders related to vscode, devextension, yarn, gobuilds etc...
-# This removes all the files in the /tmp folder that are older than 3 days.
-find /tmp -mindepth 1 -mtime +3 -exec rm -rf {} +
+# This removes all the files in the /tmp folder that are older than 8 hours.
+find /tmp -mindepth 1 -mmin +480 -exec rm -rf {} \;
